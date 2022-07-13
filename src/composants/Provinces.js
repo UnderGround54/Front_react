cc16c561a8a6c93d7836ac9ef6d606c769d5197b
UnderGround54 <martinpros54@gmail.com>
2022-07-13 08:14:12 +0300

    structure de react

---
+++ src/composants/Provinces.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const Provinces = () => {
+    const [provinces, setProvinces] = useState([]);
+    const getAllProvinces = () => {
+
+    }
+    const deleteProvince = () => {
+
+    }
+    return (
+        <div>Provinces</div>
+    )
+}
+
+export default Provinces
