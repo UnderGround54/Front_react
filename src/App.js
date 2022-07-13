fd18b68c17cb3d752682010a15fe19a5804bf086
UnderGround54 <martinpros54@gmail.com>
2022-07-13 12:01:05 +0300

    recuper les donner sur le front

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import Provinces from './composants/Provinces';
+import CreerProvince from './composants/Provinces';
+import EditProvince from './composants/EditProvince'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <p>
-        <button className='btn'> Creer </button>
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <Provinces/>} />
+          <Route path='/create' element={ <CreerProvince/>} />
+          <Route path='/:id' element={ <EditProvince/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/composants/Provinces.js
+++ src/composants/Provinces.js
@@ -1,16 +1,92 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { Grid,Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+const url_api =  'http://localhost:8000/api'
+
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
+  
+  const StyledTableRow = withStyles((theme) => ({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  }))(TableRow);
+  
+  const useStyles = makeStyles({
+      table: {
+        minWidth: 700,
+      },
+    });
 
 const Provinces = () => {
     const [provinces, setProvinces] = useState([]);
-    const getAllProvinces = () => {
 
-    }
-    const deleteProvince = () => {
+    useEffect(() => {
+        getAllProvinces()
+    },[])
 
+    const getAllProvinces = async () => {
+        const res = await axios.get(`${url_api}/provinces`)
+        setProvinces(res.data)
     }
+    const deleteProvince = async (id) => {
+        await axios.delete(`${url_api}/provinces/${id}`)
+        getAllProvinces()
+    }
+    const classes = useStyles();
+    
     return (
-        <div>Provinces</div>
+        <div>
+            <Grid container>
+                <Link to="/create" variant="contained" color="primary">Create</Link>
+            </Grid>
+            <TableContainer component={Paper}>
+                <Table  className={classes.table} aria-label="customized table">
+                    <TableHead>
+                    <TableRow>
+                        <StyledTableCell>ID</StyledTableCell>
+                        <StyledTableCell align="right">Libelle Prov</StyledTableCell>
+                        <StyledTableCell align="right">Code prov</StyledTableCell>
+                        <StyledTableCell align="right">Action</StyledTableCell>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {provinces.map((province) => (
+                        <StyledTableRow key={province.id}>
+                        <StyledTableCell component="th" scope="row">
+                            {province.id}
+                        </StyledTableCell>
+                        <StyledTableCell align="right">{province.libelle_prov}</StyledTableCell>
+                        <StyledTableCell align="right">{province.code_prov}</StyledTableCell>
+                        <StyledTableCell align="right">
+                            <Link to={`/provinces/${province.id}`} variant="contained" color="primary">Edit</Link>
+                            <Button onClick={()=> deleteProvince(province.id)} variant="contained" size="small" color="secondary">Delete</Button>
+                        </StyledTableCell>
+                        </StyledTableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
     )
 }
 
